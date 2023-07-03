2905270055f364883c637e08ce72542b9a980e46
Sanjaya.Koju <sanjaya.koju@blankfactor.com>
2023-07-02 21:06:18 -0700

    Changes

--- src/app/auth-module/auth.module.ts
+++ src/app/auth-module/auth.module.ts
@@ -5,12 +5,14 @@ import {UserRegisterComponent} from './component/user-register/user-register.com
 import {AuthRoutingModule} from './auth-routing.module';
 import {ReactiveFormsModule} from "@angular/forms";
 import {SharedModule} from "../shared/shared.module";
+import { UserListComponent } from './component/user-list/user-list.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    UserRegisterComponent
+    UserRegisterComponent,
+    UserListComponent
   ],
   imports: [
     CommonModule,
